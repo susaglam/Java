7beb61d802fbb97f9ada5c436a9c879e17b57f3e
Sukru Saglam <sukru_saglam@hotmail.com>
2020-09-19 21:09:28 +0200

    Review Days

---
+++ src/Java_Sessions/Week_1/ReviewComments.java
@@ -0,0 +1,17 @@
+package Java_Sessions.Week_1;
+
+public class ReviewComments {
+	public static void main(String[] args) {
+
+		// This is how we comment in java for single line
+
+		/*
+		 * This is how we can write multiple line of comment and it will be ignored by
+		 * compiler you can write as much as you want
+		 */
+
+		// in order to create sub package , you can just add
+		// the name as yourpackage.subpackagename.
+
+	}
+}
