81f51ac44c27025bcbcb490f861e08f79573af7e
Sukru Saglam <sukru_saglam@hotmail.com>
2020-09-23 13:49:15 +0200

    if  else statements

---
+++ src/classes/Switch_Statement.java
@@ -0,0 +1,10 @@
+package classes;
+
+public class Switch_Statement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/classes/if_ternary_operator.java
@@ -0,0 +1,23 @@
+package classes;
+
+public class if_ternary_operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String move;
+		boolean isactive = true;
+
+		if (isactive) {
+			move = "Movement started";
+		} else {
+			move = "No Movement";
+		}
+		System.out.println(move);
+
+		move = (isactive) ? "Movement Started 2" : "No Movement 2";
+		System.out.println(move);
+
+	}
+
+}
