8b6fc73df88eb7a6e308dc0e69351637cbb7957f
Sukru Saglam <sukru_saglam@hotmail.com>
2020-09-24 15:23:01 +0200

    Update Question6.java

--- src/assignment/Arithmetic_Operators/Question6.java
+++ src/assignment/Arithmetic_Operators/Question6.java
@@ -1,5 +1,10 @@
 package assignment.Arithmetic_Operators;
 
+import java.util.Scanner;
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.sym;
+import com.sun.xml.internal.ws.api.pipe.NextAction;
+
 public class Question6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -17,10 +22,18 @@ public class Question6 {
 		 * Number of milligrams in drink: 500 It would take about 20 drinks for a lethal
 		 * overdose
 		 */
-		
-		
-		
 
-		System.out.println("Number of milligrams in drink: 500 It would take about 20 drinks for a lethal overdose");
+		int drinks;
+		int totalDrinkMl; // Dont remember miligram to gram
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Number of milligrams in drink: ");
+
+		totalDrinkMl = input.nextInt();
+
+		drinks=10000/totalDrinkMl;
+
+		System.out.println("Number of milligrams in drink: " + totalDrinkMl + " It would take about " + drinks
+				+ " drinks for a lethal overdose");
 	}
 }
