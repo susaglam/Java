97d391e1a784e1242bb131529b42dfe00ff2e63f
Sukru Saglam <sukru_saglam@hotmail.com>
2020-10-21 22:49:12 +0200

    lab2 update

---
+++ src/assignments/Lab2/Lab2_4_Display_PercentageOf_MalesAndFemales.java
@@ -0,0 +1,32 @@
+package assignments.Lab2;
+
+import java.util.Scanner;
+
+/*
+Question-3
+
+Write a program that asks the user for a
+positive nonzero integer value.
+The program should use a loop to get the sum of
+all the integers from 1 up to the number entered.
+*/
+public class Lab2_4_Display_PercentageOf_MalesAndFemales {
+    public static void main (String[] args) {
+        int numberOfMales = 0, numberOfFemales = 0, nextNumber = 0, totalPerson;
+        double percentageOfMales, percentageOfFemales;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number of males: ");
+        numberOfMales = scan.nextInt();
+        System.out.print("Enter number of females: ");
+        numberOfFemales = scan.nextInt();
+        totalPerson = numberOfFemales + numberOfMales;
+        percentageOfFemales = ((double)numberOfFemales/(double)totalPerson) / 100;
+        percentageOfMales = ((double)numberOfMales/(double)totalPerson) / 100;
+
+
+
+        System.out.print("Eaten total calories  : " + percentageOfMales);
+        System.out.print("Eaten total calories  : " + percentageOfMales);
+        scan.close();
+    }
+}
