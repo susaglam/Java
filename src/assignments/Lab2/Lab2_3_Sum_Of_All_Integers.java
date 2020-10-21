da9f656a2e8967821d7630c1950d77f585ab8250
Sukru Saglam <sukru_saglam@hotmail.com>
2020-10-21 22:49:04 +0200

    lab2 update

--- src/assignments/Lab2/Lab2_3_Sum_Of_All_Integers.java
+++ src/assignments/Lab2/Lab2_3_Sum_Of_All_Integers.java
@@ -1,17 +1,18 @@
 package assignments.Lab2;
+
 import java.util.Scanner;
 
-    /*
-    Question-3
+/*
+Question-3
 
-    Write a program that asks the user for a
-    positive nonzero integer value.
-    The program should use a loop to get the sum of
-    all the integers from 1 up to the number entered.
-    */
+Write a program that asks the user for a
+positive nonzero integer value.
+The program should use a loop to get the sum of
+all the integers from 1 up to the number entered.
+*/
 public class Lab2_3_Sum_Of_All_Integers {
     public static void main (String[] args) {
-int numbers = 0, nextNumber = 0;
+        int numbers = 0, nextNumber = 0;
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a positive nonzero integer number: ");
         numbers += scan.nextInt();
@@ -23,11 +24,11 @@ int numbers = 0, nextNumber = 0;
             System.out.print("Do you want to enter another number : 0-NO   1-YES ");
             nextNumber = scan.nextInt();
 
-            if (nextNumber!=1)
+            if (nextNumber != 1)
                 break;
         } while (nextNumber == 1);
 
-        System.out.print("Eaten total calories  : " + numbers);
+        System.out.print("Sum of total all numbers  : " + numbers);
         scan.close();
     }
 }
