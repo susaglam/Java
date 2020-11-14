800b9df52e1b433b0d99ca1f5f2fc937b96ca68c
Sukru Saglam <sukru_saglam@hotmail.com>
2020-11-14 04:37:34 +0100

    added new method and explanation

--- src/Interview_Question/frequencyOfCharacters.java
+++ src/Interview_Question/frequencyOfCharacters.java
@@ -1,35 +1,45 @@
 package Interview_Question;
 
+import java.util.Arrays;
+
 public class frequencyOfCharacters {
-    /*
-            String -- Frequency of Characters Write a return method that can find the
-		 * frequency of characters
-		 * -->: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
-         */
+    /*String -- Frequency of Characters Write a return method that can find the
+     * frequency of characters
+     * -->: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+     */
     public static void main (String[] args) {
-        System.out.println(FrequencyOfChars("AAABBCDD"));
-        // ==> A3B2C1D2
+        System.out.println(FrequencyOfChars("AAABBCDD"));        // ==> A3B2C1D2
     }
 
     public static String FrequencyOfChars (String str) {
         String unique = "";
+        str = sort(str); // sorting string
+        int c = 1;
         for (int i = 0; i < str.length(); i++) {
-
-            if (!unique.contains(str.charAt(i) + "")) {
-                unique += str.charAt(i);
+            //We compare every char with next char. str.charAt(i) == str.charAt(i + 1)
+            //if they are equal increase the count
+            //if they are not equal then write the letter and count
+            //I used str.charAt(i + 1) for found next char
+            // but if (i + 1) equal to str.length  then  i used ternary operator =>      (i + 1) == str.length() ? str.length() - 1
+            // and write the last char and count
+            if (str.charAt(i) == ((i + 1) == str.length() ? str.length() - 1 : str.charAt(i + 1))) {
+                c++;
+            } else {
+                unique += str.charAt(i) + "" + c;
+                c = 1;
             }
         }
+        return unique;
+    }
 
-        String result = "";
-        for (int j = 0; j < unique.length(); j++) {
-            int count = 0;
-            for (int k = 0; k < str.length(); k++) {
-                if (str.charAt(k) == unique.charAt(j)) {
-                    count++;
-                }
-            }
-            result += unique.charAt(j) + "" + count;
-        }
-        return result;
+    // Method to sort a string alphabetically
+    public static String sort (String str) {
+        // convert string to char array
+        char temp[] = str.toCharArray();
+
+        // sorting
+        Arrays.sort(temp);
+        //and returning
+        return new String(temp);
     }
 }
