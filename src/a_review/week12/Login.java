8e5b1f5c2a2cee00c473d23f90d4440e1c82f564
Sukru Saglam <sukru_saglam@hotmail.com>
2020-12-06 13:30:02 +0100

    week 12

--- src/a_review/week12/Login.java
+++ src/a_review/week12/Login.java
@@ -2,16 +2,29 @@ package a_review.week12;
 
 public class Login {
 
-	public static void main(String[] args) {
-		Credentials obj1 = new Credentials();
-		System.out.println("UserName entered :"+ obj1.getUserName());
-		System.out.println("Password entered :"+ obj1.getPassWord());
-		
-		obj1.setUserName("newuser");
-		obj1.SetPassWord("newpass");
-		System.out.println("___________________________________________" );
-		System.out.println("New getUserName entered :"+ obj1.getUserName() );
-		System.out.println("New Password entered :"+ obj1.getPassWord() );
-	}
+    public static void main(String[] args) {
+        Credentials obj1 = new Credentials();
+		 /*
+        System.out.println("Username entered: "+ obj1.userName); // not visible
+        System.out.println("Password entered: "+ obj1.passWord); // not visible
+
+        System.out.println("=========================================");
+        obj1.userName = "B123456"; // not visible
+        obj1.passWord = "789456"; // not visible
+
+        System.out.println("Username entered: "+ obj1.userName); // not visible
+        System.out.println("Password entered: "+ obj1.passWord); // not visible
+    */
+
+
+        System.out.println("UserName entered :" + obj1.getUserName());
+        System.out.println("Password entered :" + obj1.getPassWord());
+
+        obj1.setUserName("newuser");
+        obj1.SetPassWord("newpass");
+        System.out.println("___________________________________________");
+        System.out.println("New getUserName entered :" + obj1.getUserName());
+        System.out.println("New Password entered :" + obj1.getPassWord());
+    }
 
 }
